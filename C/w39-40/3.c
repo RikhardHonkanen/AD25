9928549df52564cd7f784e38d80fe650dbf75bc2
RikhardHonkanen <rikhard@raket.co>
2025-09-23 14:29:12 +0200

    excersises

---
+++ C/w39-40/3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * @brief
+ * Prints "Hello World"
+ *
+ * @return int
+ */
+int main(void)
+{
+    printf("Hello world!\n");
+
+    return 0;
+}
---
+++ C/w39-40/4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * @brief
+ * Prints "Hello World"
+ *
+ * @return int
+ */
+int main(void)
+{
+#ifdef NUMBER
+#if (NUMBER != 8) && (NUMBER != 16)
+#error "NUMBER is not 8 or 16"
+#endif
+#else
+#error "NUMBER is not defined!"
+#endif
+
+    return 0;
+}
