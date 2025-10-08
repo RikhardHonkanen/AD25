ce7eabda0c21c0c3b965dbeb6547e884d2e60f0b
RikhardHonkanen <rikhard@raket.co>
2025-10-08 18:06:06 +0200

    comments

--- Python/kattis/permuted_arithmetic_sequence.py
+++ Python/kattis/permuted_arithmetic_sequence.py
@@ -12,26 +12,26 @@ def is_arithmetic(seq):
     return all(seq[i+1] - seq[i] == diff for i in range(len(seq)-1))
 
 def main(input):  
-    clean = [' '.join(s.split()[1:]) for s in input if len(s.split()) > 1]
+    clean = [' '.join(s.split()[1:]) for s in input if len(s.split()) > 1]  # Get rid of useless info
     num_arrays = []
-    for s in clean:
+    for s in clean: # Sort numbers into arrays so we can use the set() function later
         numbers = [int(x) for x in s.split()]
         num_arrays.append(numbers)
         
-    print("numarrays: ", num_arrays)
-        
-    for arr in num_arrays:
-        arr_set_sorted = []
+    arr_set_sorted = []
+    for arr in num_arrays:  # Sweep 1
         if is_arithmetic(arr):
             print(arr, "is already arithmetic")
         else:
-            arr_set_sorted = sorted(set(arr))
-        if is_arithmetic(arr_set_sorted):
-            print(arr, "can be reordered to arithmetic:", arr_set_sorted)
+            arr_set_sorted.append(sorted(set(arr)))
+            
+    for arr in arr_set_sorted: # Sweep 2 after set()
+        if is_arithmetic(arr):
+            print(arr, "can be reordered to arithmetic:", arr)
         else:
             print(arr, "cannot form an arithmetic sequence")
 
-    return "woot"
+    return 0
 
 if __name__ == "__main__":
     input = parse_file("permuted_arithmetic_sequence.txt")
