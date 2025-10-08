9ab2647e8be67bd1ec168707394e237597085a1a
RikhardHonkanen <rikhard@raket.co>
2025-10-08 17:46:20 +0200

    kattis permuted_arithmetic_sequence

---
+++ Python/kattis/permuted_arithmetic_sequence.py
@@ -0,0 +1,40 @@
+import os, sys
+
+def parse_file(path):    
+	with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), path), "r") as f:
+		parsed_input = f.read().split('\n')
+	return parsed_input
+
+def is_arithmetic(seq):
+    if len(seq) < 2:
+        return True  # A single number or empty sequence is trivially arithmetic
+    diff = seq[1] - seq[0]
+    return all(seq[i+1] - seq[i] == diff for i in range(len(seq)-1))
+
+def main(input):  
+    clean = [' '.join(s.split()[1:]) for s in input if len(s.split()) > 1]
+    num_arrays = []
+    for s in clean:
+        numbers = [int(x) for x in s.split()]
+        num_arrays.append(numbers)
+        
+    print("numarrays: ", num_arrays)
+        
+    for arr in num_arrays:
+        arr_set_sorted = []
+        if is_arithmetic(arr):
+            print(arr, "is already arithmetic")
+        else:
+            arr_set_sorted = sorted(set(arr))
+        if is_arithmetic(arr_set_sorted):
+            print(arr, "can be reordered to arithmetic:", arr_set_sorted)
+        else:
+            print(arr, "cannot form an arithmetic sequence")
+
+    return "woot"
+
+if __name__ == "__main__":
+    input = parse_file("permuted_arithmetic_sequence.txt")
+    print()
+    print(f"Output: {main(input)}")
+    
