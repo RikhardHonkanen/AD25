4d7a9210ce2e5d487ac944067207cf9f7b4d303c
Rikhard Honkanen <rikhard.honkanen@mail.com>
2025-11-18 13:48:36 +0100

    w47-49 40

---
+++ C/w47-49/40.c
@@ -0,0 +1,73 @@
+/**
+ * @file 40.c
+ * @author Rikhard Honkanen (rikhard.honkanen@mail.com)
+ * @brief Study the behaviour of the strtok function in string.h; http://www.cplusplus.com/reference/cstring/strtok/
+ * Make a stand-alone and self-contained function like strtok to split a string using a delimiter which is one character.
+ * @version 0.1
+ * @date 2025-11-18
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char *split_string(const char *str, const char delim);
+
+static char *split_string(const char *str, const char delim)
+{
+    static const char *p = NULL;
+
+    if (str != NULL)
+    {
+        p = str;
+    }
+
+    if (p == NULL || *p == '\0')
+    {
+        return NULL;
+    }
+
+    const char *start = p;
+
+    while (*p && *p != delim)
+    {
+        p++;
+    }
+
+    size_t len = p - start;
+    char *token = malloc(len + 1);
+    memcpy(token, start, len);
+    token[len] = '\0';
+
+    if (*p == delim)
+    {
+        p++;
+    }
+
+    return token;
+}
+
+int main(void)
+{
+    char *token;
+    char *tokens[32];
+    int count = 0;
+
+    char *str = "Sarah,Bob,Evan";
+    while ((token = split_string(str, ',')) != NULL)
+    {
+        tokens[count++] = token;
+        str = NULL;
+    }
+
+    (void)printf("Tokens:\n");
+    for (size_t i = 0; i < count; i++)
+    {
+        (void)printf("%s\n", tokens[i]);
+        (void)free(tokens[i]);
+    }
+
+    return 0;
+};
