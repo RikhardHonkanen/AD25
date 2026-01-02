b41ad0c9f72205e770e58afd346c550acb6eebbf
Rikhard Honkanen <rikhard.honkanen@mail.com>
2026-01-02 10:20:31 +0100

    14

---
+++ C++/w50-51/14-guessing-game.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+constexpr int GUESSES{10};
+constexpr int MAX_NUM{100};
+
+void greet(void);
+void play_game(void);
+
+void greet(void)
+{
+    std::cout << "******************************************\n\n************** Game Started **************\n\n******************************************\n";
+}
+
+void play_game(void)
+{
+    int secret = rand() % MAX_NUM;
+    int guesses = GUESSES;
+    bool win = false;
+
+    int guess;
+    while (!win && guesses > 0)
+    {
+        std::cout << guesses << " guesses left.\n";
+        do
+        {
+            std::cout << "Guess a number [0, 99]: ";
+            std::cin >> guess;
+        } while (0 > guess || guess >= MAX_NUM);
+
+        if (guess == secret)
+        {
+            win = true;
+        }
+        else if (guess < secret)
+        {
+            std::cout << "Sorry, too small. Try again!\n";
+        }
+        else
+        {
+            std::cout << "Sorry, too big. Try again!\n";
+        }
+
+        --guesses;
+    }
+
+    if (win)
+    {
+        std::cout << "Congratulations, you guessed the secret!\n";
+    }
+    else
+    {
+        std::cout << "Unfortunately, you did not guess the secret.\n";
+    }
+
+    char again;
+    do
+    {
+        std::cout << "Play again [Y/N]? ";
+        std::cin >> again;
+    } while (again != 'Y' && again != 'y' && again != 'N' && again != 'n');
+
+    if (again == 'Y' || again == 'y')
+    {
+        play_game();
+    }
+}
+
+int main(void)
+{
+    srand(time(nullptr));
+    (void)greet();
+    (void)play_game();
+    std::cout << "\nThanks for playing!\n";
+
+    return 0;
+}
